d9d77cd2f66994237c985df1091d9af697355019
Sander Grimstad <sander.grimstad00@gmail.com>
2024-03-07 10:56:51 +0100

    Added Spring boot project to backend folder

---
+++ backend/src/main/java/no/ntnu/webappgroup03/WebAppGroup03Application.java
@@ -0,0 +1,13 @@
+package no.ntnu.webappgroup03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppGroup03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppGroup03Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/no/ntnu/webappgroup03/WebAppGroup03ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.webappgroup03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppGroup03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
