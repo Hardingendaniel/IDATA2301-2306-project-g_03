4423f75728b1f7b9561999bc03cc03038179eb77
Hardingendaniel <danieae@stud.ntnu.no>
2024-03-13 22:28:04 +0100

    The arrow switching hotel-cars is now placed on the images. pluss minor changes

--- frontend/Main/Script.js
+++ frontend/Main/Script.js
@@ -9,29 +9,28 @@ var span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
 if (btn) {
-    btn.addEventListener('click', function() {
+    btn.addEventListener('click', function () {
         modal.style.display = 'block';
     });
 }
 
 
-
 // When the user clicks on (x), close the modal
 if (span) {
-    span.addEventListener('click', function() {
+    span.addEventListener('click', function () {
         modal.style.display = 'none';
     });
 }
 
 // When the user clicks outside of the modal, close the modal
-window.addEventListener('click', function(event) {
+window.addEventListener('click', function (event) {
     if (event.target == modal) {
         modal.style.display = 'none';
     }
 });
 
 // Close modal with ESC key
-window.addEventListener('keydown', function(event) {
+window.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
         modal.style.display = 'none';
     }
