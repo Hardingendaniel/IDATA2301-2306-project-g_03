bb3d9aa68411770f409ea03423050bbe25372747
Daniel Eikås <danieae@stud.ntnu.no>
2024-03-18 11:27:14 +0100

    Improved the buttons on the review carousel

--- frontend/Main/Script2.js
+++ frontend/Main/Script2.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll('.card');
     let currentIndex = 0;
+    const shufflebackwards = document.getElementById("Shuffle_reviews2");
 
     function showCards(index) {
         cards.forEach(card => {
@@ -14,12 +15,26 @@ document.addEventListener('DOMContentLoaded', () => {
         cards[index].style.display = 'block';
         cards[nextIndex1].style.display = 'block';
         cards[nextIndex2].style.display = 'block';
+
+        if (index === 0) {
+            shufflebackwards.style.display = 'none';
+        } else {
+            shufflebackwards.style.display = 'block';
+        }
+
     }
     showCards(currentIndex);
 
     document.getElementById('Shuffle_reviews').addEventListener('click', () => {
         currentIndex = (currentIndex + 1) % cards.length;
         showCards(currentIndex);
+    });
+
+    document.getElementById('Shuffle_reviews2').addEventListener('click',()=> {
+        if (currentIndex > 0) {
+            currentIndex = (currentIndex - 1) %cards.length;
+            showCards(currentIndex);
+        }
     });
 });
 
