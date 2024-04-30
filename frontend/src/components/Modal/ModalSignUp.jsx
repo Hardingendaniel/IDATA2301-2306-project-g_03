7052e69444c2a3623fd4e8b82d8ac401d2ff2c4a
Sten <stenhalse@gmail.com>
2024-04-30 11:08:17 +0200

    Fixed an error in modal with buttons

--- frontend/src/components/Modal/ModalSignUp.jsx
+++ frontend/src/components/Modal/ModalSignUp.jsx
@@ -22,7 +22,7 @@ export function SignUpWindow() {
                 onClick={handleShow}
                 className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded"
             >
-                Log in
+                Not a member? Sign up
             </button>
 
             {/* Modal code */}
