51f0b083120a839fdf0e6373ad58d79a222e0934
Sten <stenhalse@gmail.com>
2024-04-30 11:46:24 +0200

    Added phoneNumber to sign up

--- frontend/src/components/Modal/Modal2.jsx
+++ frontend/src/components/Modal/Modal2.jsx
@@ -65,7 +65,7 @@ export function ModalOpener1() {
                         type="submit"
                         className="w-full bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded my-3"
                     >
-                        Sign in
+                        Log In
                     </btn>
                     <div className="flex items-center justify-between my-4">
                         <hr className="w-full border-t border-gray-300" />
