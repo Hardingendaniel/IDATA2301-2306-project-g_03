9fe8210aad12b06772a983883d31a0fac679890e
Sten <stenhalse@gmail.com>
2024-05-02 10:38:21 +0200

    Added a backend class CorsConfig for recieving requests from frontEnd

---
+++ backend/src/main/java/no/ntnu/webappgroup03/controllers/CorsConfig.java
@@ -0,0 +1,15 @@
+package no.ntnu.webappgroup03.controllers;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+  public void addCorsMapping (CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+  }
+}
