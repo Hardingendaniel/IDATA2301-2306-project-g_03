b4252600ca66a9139c07add2a080d0d769895ec8
Sten <stenhalse@gmail.com>
2024-05-15 12:31:41 +0200

    Implementing fetch function in ProductCard and communication with Backend

--- frontend/src/pages/hotelpage/HotelPage.jsx
+++ frontend/src/pages/hotelpage/HotelPage.jsx
@@ -19,7 +19,7 @@ export function HotelPage() {
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await fetch("http://localhost:8080/api/hotels/2");
+                const response = await fetch("http://localhost:8080/api/hotels/4");
                 const data = await response.json();
                 setData2(data);
             } catch (error) {
